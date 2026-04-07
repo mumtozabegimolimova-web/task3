b4ce01ef1301829682a98704cff7609015f9f532
mumtozabegimolimova-web <mumtozabegimolimova@gmail.com>
2026-04-08 00:37:59 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+@app.route('/<email>', methods=['GET'])
+def calculate(email):
+    try:
+        x = request.args.get('x')
+        y = request.args.get('y')
+
+        if x is None or y is None:
+            return "NaN"
+
+        x = int(x)
+        y = int(y)
+
+        if x < 0 or y < 0:
+            return "NaN"
+
+        return str(lcm(x, y))
+    except:
+        return "NaN"
